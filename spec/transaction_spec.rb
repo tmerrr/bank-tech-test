c50b7ea5e308a0b5f98c18064abd9d107b15c036
Tom Moir <trmoir9@gmail.com>
2018-01-22 15:22:41 +0000

    updated tests for Transaction to replace Time with a double

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,10 +1,15 @@
 require 'transaction'
 
 describe Transaction do
+  let(:time) { double(:time) }
+  before(:each) do
+    allow(Time).to receive(:now).and_return(time)
+  end
+
   describe '#print_details' do
     context 'when the transaction is a £10 deposit and balance is £20' do
       it 'prints the details of the transaction' do
-        allow(Time).to receive_messages(now: Time.new(2018, 1, 2))
+        allow(time).to receive(:strftime).and_return('02/01/2018')
         transaction = described_class.new(credit: 10, balance: 20)
         expect { transaction.print_details }
           .to output("02/01/2018 || 10.00 ||  || 20.00\n").to_stdout
@@ -13,7 +18,7 @@ describe Transaction do
 
     context 'when the transaction is a £30 deposit and balance is £50' do
       it 'prints the details of the transaction' do
-        allow(Time).to receive(:now).and_return(Time.new(2018, 1, 5))
+        allow(time).to receive(:strftime).and_return('05/01/2018')
         transaction = described_class.new(credit: 30, balance: 50)
         expect { transaction.print_details }
           .to output("05/01/2018 || 30.00 ||  || 50.00\n").to_stdout
@@ -22,7 +27,7 @@ describe Transaction do
 
     context 'when the transaction is a £25 withdrawal and balance is £65.50' do
       it 'prints the details of the transaction' do
-        allow(Time).to receive(:now).and_return(Time.new(2017, 8, 22))
+        allow(time).to receive(:strftime).and_return('22/08/2017')
         transaction = described_class.new(debit: 25, balance: 65.5)
         expect { transaction.print_details }
           .to output("22/08/2017 ||  || 25.00 || 65.50\n").to_stdout
@@ -31,7 +36,7 @@ describe Transaction do
 
     context 'when the transaction is £65.23 withdrawal and balance is £4.77' do
       it 'prints the details of the transaction' do
-        allow(Time).to receive(:now).and_return(Time.new(2017, 10, 12))
+        allow(time).to receive(:strftime).and_return('12/10/2017')
         transaction = described_class.new(debit: 65.23, balance: 4.77)
         expect { transaction.print_details }
           .to output("12/10/2017 ||  || 65.23 || 4.77\n").to_stdout
